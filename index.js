a5850bb4041ee7ebbfce0af33a6891825cd77a8b
pashkovnk <pashkov.nikita03@mail.ru>
2023-06-03 18:03:42 +0300

    Добавил Middleware для CORS

--- index.js
+++ index.js
@@ -118,3 +118,14 @@ app.delete('/api/todos', async (req, res) => {
 app.listen(PORT, () => {
     console.log(`Сервер запущен на порту ${PORT}`);
 });
+
+// Middleware для обработки JSON данных
+app.use(express.json());
+
+//Middleware для CORS
+app.use((req, res, next) => {
+res.setHeader('Access-Control-Allow-Origin', '*');
+res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE');
+res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+next();
+});
